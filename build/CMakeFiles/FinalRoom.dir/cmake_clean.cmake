75c8ef9568cb4a46e4803f717f144b60cdc39583
maangulaid <aabdilahi132@gmail.com>
2025-11-21 11:08:02 -0600

    Phase 3a fix: corrected table alignment & positioning

---
+++ build/CMakeFiles/FinalRoom.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalRoom.dir/src/main.cpp.obj"
+  "CMakeFiles/FinalRoom.dir/src/main.cpp.obj.d"
+  "FinalRoom.exe"
+  "FinalRoom.exe.manifest"
+  "FinalRoom.pdb"
+  "libFinalRoom.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalRoom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
