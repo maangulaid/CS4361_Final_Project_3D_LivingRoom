d1546ace3d8a46bc81a9b833b80eec3221fd8bba
Sarayu <164818616+SarayuGajula@users.noreply.github.com>
2025-11-23 21:36:36 -0600

    Merged remote changes

---
+++ build/CMakeFiles/FinalRoom.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalRoom.dir/src/Cube.cpp.o"
+  "CMakeFiles/FinalRoom.dir/src/Cube.cpp.o.d"
+  "CMakeFiles/FinalRoom.dir/src/glad.c.o"
+  "CMakeFiles/FinalRoom.dir/src/glad.c.o.d"
+  "CMakeFiles/FinalRoom.dir/src/main.cpp.o"
+  "CMakeFiles/FinalRoom.dir/src/main.cpp.o.d"
+  "FinalRoom"
+  "FinalRoom.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/FinalRoom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
